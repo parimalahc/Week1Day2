e615086cb4accd6acd16c081b3feb7cc9528282b
parimalahc <42540345+parimalahc@users.noreply.github.com>
2026-07-08 11:00:17 +0530

    Add files via upload

---
+++ homeAssignment1.js
@@ -0,0 +1,20 @@
+let userName = "Parimala"
+console.log(userName)
+console.log(typeof userName)
+
+let organization = `Wipro`
+console.log(organization)
+console.log(typeof organization)
+
+let contactNumber = 9884451897
+console.log(contactNumber)
+console.log(typeof contactNumber)  
+
+let knowsAutomation = true
+console.log(knowsAutomation)
+console.log(typeof knowsAutomation)
+
+let usesPlaywright
+console.log(usesPlaywright)
+console.log(typeof usesPlaywright)
+
