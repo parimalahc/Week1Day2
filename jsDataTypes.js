b18e119b1a4150e824c47243694c2ec3adf2602e
parimalahc <42540345+parimalahc@users.noreply.github.com>
2026-08-05 10:07:46 +0530

    Add files via upload

---
+++ jsDataTypes.js
@@ -0,0 +1,25 @@
+/*
+Alt+Shift+A, comment multiple lines of code
+ctrl +/, comment single line of code
+
+*/
+
+var firstName = "Parimala"
+console.log(firstName)
+console.log(typeof firstName)
+
+var companyName=`Wipro`
+console.log(companyName)
+console.log(typeof companyName)
+
+var mobileNumber= 9884451897
+console.log(mobileNumber)
+console.log(typeof mobileNumber)
+
+var isAutomation=true
+console.log(isAutomation)
+console.log(typeof isAutomation)
+
+var hasPlaywright
+console.log(hasPlaywright)
+console.log(typeof hasPlaywright)
